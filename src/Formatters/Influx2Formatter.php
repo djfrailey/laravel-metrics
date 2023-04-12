bce06b4e20eed6b68ac52b823a9d074a2a8142a2
David Frailey <endl@pop-os.localdomain>
2023-04-12 13:52:56 -0700

    Bypass adapter

--- src/Formatters/Influx2Formatter.php
+++ src/Formatters/Influx2Formatter.php
@@ -2,28 +2,19 @@
 
 namespace STS\Metrics\Formatters;
 
-use STS\Metrics\Adapters\InfluxDB2Adapter;
 use STS\Metrics\Contracts\Formatter;
 use STS\Metrics\Metric;
 
 class Influx2Formatter implements Formatter
 {
-
-    protected $adapter;
-
-    public function __construct(InfluxDB2Adapter $adapter)
-    {
-        $this->adapter = $adapter;
-    }
-
     public function format(Metric $metric)
     {
-        return $this->adapter->point(
+        return (new \InfluxDB2\Point(
             $metric->getName(),
             $metric->getValue(),
             $metric->getTags(),
             $metric->getExtra(),
             $metric->getTimestamp()
-        )->toLineProtocol();
+        ))->toLineProtocol();
     }
 }
